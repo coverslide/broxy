ed1384b57b6196e48ea8edab46171f9a44f86925
Richard Hoffman <coverslide@gmail.com>
2012-11-01 16:05:14 -0700

    add setuid for dropping permissions

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function main(){
     .option('-a, --address <address>', 'ip address to listen on')
     .option('-s, --secure-port <port>', 'port to listen to secure traffic from')
     .option('-A, --secure-address <address>', 'port to listen to secure traffic from')
+    .option('-u, --setuid <uid>', 'uid to drop permissions to')
     .parse(process.argv)  
 
   var configFile = app.args[0] || 'broxy.json'
@@ -33,10 +34,12 @@ function main(){
     config = {}
 
   //
-  ;['keyFile', 'certFile', 'key', 'cert', 'forward', 'port', 'address', 'securePort', 'secureAddress'].forEach(function(key){
+  ;['keyFile', 'certFile', 'key', 'cert', 'forward', 'port', 'address', 'securePort', 'secureAddress', 'setuid'].forEach(function(key){
     if(app[key])
       config[key] = app[key]
   })
+
+  configDir = path.dirname(configPath)
 
   if(!config.port)
     config.port = 80
@@ -44,14 +47,17 @@ function main(){
     config.securePort = 443
 
   if(config.keyFile)
-    config.key = fs.readFileSync(config.keyFile)
+    config.key = fs.readFileSync(path.resolve(configDir, config.keyFile))
   if(config.certFile)
-    config.cert = fs.readFileSync(config.certFile)
+    config.cert = fs.readFileSync(path.resolve(configDir, config.certFile))
 
   var proxies = init(config)
 
   proxies.proxy && proxies.proxy.listen(config.port, config.address)
   proxies.secureProxy && proxies.secureProxy.listen(config.securePort, config.secureAddress)
+
+  if(config.setuid)
+    process.setuid(config.setuid)
 
   return proxies
 }
@@ -77,15 +83,15 @@ function init(config){
 //mostly useless on its own
 function broxy(config){
 
+  function onrequest(req, bounce){
+    bounce(config.forward, config.host)
+  }
+
   if(config.key && config.cert){
-    var server = bouncy({key: config.key, cert: config.cert}, function(req, bounce){
-      bounce(config.forward, config.host)
-    })
+    var server = bouncy({key: config.key, cert: config.cert}, onrequest)
   }
   else{
-    var server = bouncy(function(req, bounce){
-      bounce(config.forward, config.host)
-    })
+    var server = bouncy(onrequest)
   }
 
   return server
